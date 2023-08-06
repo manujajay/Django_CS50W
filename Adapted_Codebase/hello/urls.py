f5f03b3ff641164c6e15188a6078c100d412f32a
manujajay <manujajayawardanais@gmail.com>
2023-08-06 21:14:59 +0100

    Leaving the original code base in

--- Adapted_Codebase/hello/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from . import views # . means the current directory
-
-urlpatterns = [
-    path("", views.index, name="index"), # "" means the root path of the application (i.e. http:// localhost:8000/) and
-    # views.index means the index function in the views.py file of the hello application 
-]
